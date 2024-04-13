dfebc8d11935b270d8aecd195f12d3af82cd952e
OneDuckyBoy <stilianmatev@gmail.com>
2024-04-13 14:57:41 +0300

    fixed unit and ui tests

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
+    await page.goto('https://stilian-matev-regular-exam.onrender.com/');
     const heading = await page.$('Title');
     const text = await heading.textContent();
     expect(text).toContain('Cookbook');  
--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check cookbook page', async ({ page }) => {
-    await page.goto('http://localhost:8080/Cookbook');
+    await page.goto('https://stilian-matev-regular-exam.onrender.com/Cookbook');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
